8559ba64858f49e272184375acb52ec73cee0c23
Ugo <deluciaugo@gmail.com>
2024-03-08 22:59:56 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert, View, Button, FlatList, Text, Modal } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { useEffect, useState } from 'react';
+import { Entypo } from '@expo/vector-icons';
+import CreateFolderModal from './createFolderModal';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	const [folders, setFolders] = useState([]);
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [updateView,setUpdateView] = useState(false);
+
+	const handleCreateFolder = async (folderName) => {
+		try {
+			// Path della nuova cartella
+			const folderPath = `${FileSystem.documentDirectory}/documentP/` + folderName;
+			// Creazione della cartella
+			setUpdateView(true);
+			await FileSystem.makeDirectoryAsync(folderPath, { intermediates: true });
+		} catch (error) {
+			console.error('Errore durante la creazione della cartella:', error);
+		}
+	};
+
+	const checkAndCreateFolder = async () => {
+		try {
+			const documentPFolderExists = await FileSystem.getInfoAsync(
+				`${FileSystem.documentDirectory}/documentP`
+			);
+			if (!documentPFolderExists.exists) {
+				await FileSystem.makeDirectoryAsync(
+					`${FileSystem.documentDirectory}/documentP`,
+					{ intermediates: true }
+				);
+			} else {
+
+				fetchFolders();
+			}
+
+		} catch (error) {
+			console.error('Errore durante il controllo/creazione della cartella:', error);
+		}
+	};
+
+	const fetchFolders = async () => {
+		try {
+			const documentPDirectory = `${FileSystem.documentDirectory}/documentP`;
+			const documentPFolders = await FileSystem.readDirectoryAsync(
+				documentPDirectory
+			);
+			setFolders(documentPFolders);
+		} catch (error) {
+			console.error('Errore durante il recupero delle cartelle:', error);
+		}
+	};
+
+	const renderFolder = ({ item }) => (
+		<View style={styles.folders}>
+			<Entypo style={{ marginLeft: 10 }} name="folder" size={24} color="#1E90FF" />
+			<Text style={{ marginLeft: 10 }} >{item}</Text>
+		</View>
+	)
+
+	useEffect(() => {
+		checkAndCreateFolder();
+	}, []);
+
+	useEffect(() => {
+		fetchFolders();
+		setUpdateView(false);
+	}, [updateView]);
+
+	return (
+		<View style={{ flex: 1, marginTop: 100 }}>
+			{folders.length > 0 ? (
+				<View >
+					<FlatList
+						data={folders}
+						renderItem={renderFolder}
+						vertical={true}
+						keyExtractor={(item, index) => index.toString()}
+					/>
+					<View style={{ width: '100%', alignItems: 'center', marginTop: 100 }}>
+						<View style={{ width: '50%' }}>
+							<Button style={{ with: 20 }} title="Crea Cartella" onPress={()=>setIsModalVisible(!isModalVisible)} />
+						</View>
+					</View>
+					<CreateFolderModal
+						visible={isModalVisible}
+						onClose={() => setIsModalVisible(false)}
+						onCreateFolder={handleCreateFolder}
+					/>
+				</View>
+			) : (
+				<></>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	folders: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginVertical: 5,
+
+	}
+
 });
